8caa92f4dd24c9e42c98df79e10ad0599f051de7
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2022-12-04 13:19:00 +0800

    Create day4.py

---
+++ 2022/day4.py
@@ -0,0 +1,18 @@
+# 2752 / 1550
+with open("in.txt") as _:
+    f = _.read().split("\n")
+    IN = [x for x in f]
+
+count = 0
+count2 = 0
+for line in IN:
+    pair1, pair2 = line.split(',')
+    low, high = pair1.split('-')
+    low2, high2 = pair2.split('-')
+    if all(x in range(int(low), int(high) + 1) for x in range(int(low2), int(high2) + 1)) \
+        or all(x in range(int(low2), int(high2) + 1) for x in range(int(low), int(high) + 1)):
+        count += 1
+    if any(x in range(int(low), int(high) + 1) for x in range(int(low2), int(high2) + 1)) \
+       or any(x in range(int(low2), int(high2) + 1) for x in range(int(low), int(high) + 1)):
+        count2 += 1
+print(count, count2)
